f8ef045058e8f967dd6d3aa0cbbe58152a51ba76
wdwhiteh <wdwhiteh@566cfc12-8a5e-35fd-9a33-820ffb9420c9>
2010-04-11 20:59:36 +0000

    update aes top

--- trunk/xilinx/multicore_aes/aes_top.vhd
+++ trunk/xilinx/multicore_aes/aes_top.vhd
@@ -55,14 +55,20 @@ end aes_top;
 architecture Behavioral of aes_top is
 	constant	NUM_ENC : integer := 1;
 	constant	NUM_DEC : integer := 1;
-begin
-	signal	i_KEY_SIZE : integer range 0 to 2;
+
+	
 	
 	type		enc_byte_array is array (NUM_ENC-1 downto 0) of std_logic_vector(7 downto 0);
+	type		enc_bit_array is array (NUM_ENC-1 downto 0) of std_logic_vector(NUM_ENC-1 downto 0);
+	type		dec_byte_array is array (NUM_DEC-1 downto 0) of std_logic_vector(7 downto 0);
+	type		dec_bit_array is array (NUM_DEC-1 downto 0) of std_logic_vector(NUM_DEC-1 downto 0);
+	
+	signal	i_KEY_SIZE : integer range 0 to 2;
+	
 	signal	enc_data_in : enc_byte_array;
 	signal	enc_data_out : enc_byte_array;
 	
-	type		enc_bit_array is std_logic_vector(NUM_ENC-1 downto 0);
+
 	signal	enc_valid_in : enc_bit_array;
 	signal	enc_valid_out : enc_bit_array;
 	signal	enc_key_ready : enc_bit_array;
@@ -70,19 +76,22 @@ begin
 	signal	enc_busy_check : enc_bit_array;
 	signal  	cur_enc_in : integer range 0 to NUM_ENC-1;
 	signal  	cur_enc_out : integer range 0 to NUM_ENC-1;
+	signal	last_ENC_VALID_I : std_logic;
 	
-	type		dec_byte_array is array (NUM_DEC-1 downto 0) of std_logic_vector(7 downto 0);
+	
 	signal	dec_data_in : dec_byte_array;
 	signal	dec_data_out : dec_byte_array;
 	
-	type		dec_bit_array is std_logic_vector(NUM_DEC-1 downto 0);
+	
 	signal	dec_valid_in : dec_bit_array;
 	signal	dec_valid_out : dec_bit_array;
 	signal	dec_key_ready : dec_bit_array;	
 	signal	dec_busy : dec_bit_array;
 	signal	dec_busy_check : dec_bit_array;
 	signal	curr_dec : integer range 0 to NUM_DEC-1;
-	
+
+begin	
+
 	EncGen : for k in NUM_ENC - 1 downto 0 generate
 	begin
 		enc_gen: aes_enc
@@ -90,7 +99,7 @@ begin
 				KEY_SIZE	=> i_KEY_SIZE
 				)
 			port map	(
-				DATA_I => enc_data_in[k],
+				DATA_I => enc_data_in(k),
 				VALID_DATA_I => enc_valid_in(k),
 				KEY_I => KEY_I,
 				VALID_KEY_I => VALID_KEY_I,
@@ -122,21 +131,19 @@ begin
 				DATA_O => dec_data_out[k]
 			);
 	end
+	
 	clk_proc : process (RESET_I, CLK_I)
 	begin
 		if( RESET_I = '1' ) then
 			enc_busy_check <= (others => '1');
 			dec_busy_check <= (others => '1');
 			n_enc_busy <= (others => '0');
-			
-		elsif rising_edge(CLK_I) then
+			n_got_new_enc <= '0';
+		else if rising_edge(CLK_I) then
 			enc_busy <= n_enc_busy;
 			got_new_enc <= n_got_new_enc;			
-			enc_data_in(cur_enc_in) <= ENC_DATA_I;
-			enc_valid_in(cur_enc_in) <= ENC_VALID_I;
-			ENC_DATA_O <= enc_data_out(cur_enc_out);
-			ENC_VALID_O <= enc_valid_out(cur_enc_out);
-			
+			last_ENC_VALID_I <= ENC_VALID_I;
+			last_ENC_DATA_I <= ENC_DATA_I;
 		end if;
 	end process;
 		
@@ -149,17 +156,33 @@ begin
 						else '0';
 	DEC_READY_O <= '1' when (dec_key_ready = dec_check and not(dec_busy = dec_busy_check)) 
 						else '0';
+	ENC_DATA_O <= enc_data_out(cur_enc_out);
+
+
+	n_got_new_enc <= '1' when ENC_VALID_I = '1' and last_ENC_VALID_I = '0' else '0';
+	
+	EncInGen : for k in NUM_ENC - 1 downto 0 generate
+	begin
+		if( k = cur_enc_in ) then
+			enc_data_in(k) <= last_ENC_DATA_I;
+			enc_valid_in(k) <= last_ENC_VALID_I;
+		else			
+			enc_data_in(k) <= (others => '0');
+			enc_valid_in(k) <= '0';
+		end if;
+	end generate;
 	
 	enc_process : process (enc_busy, got_new_enc)
 	begin
+		
+		
 		BusyGen : for k in NUM_ENC - 1 downto 0 generate
-		begin
-			if( enc_busy(k) = '0' and got_new_enc = '1' ) then
+		begin			
+			if( enc_busy(k) = '0' and got_new_enc = '1' ) then				
 				n_enc_busy(k) <= '1';
-				n_got_new_enc <= 0;
-				cur_enc <= k;
-			end if;			
-		end
+				cur_enc_in <= k;			
+			end if;					
+		end generate;
 	end process;
 	
 end Behavioral;
